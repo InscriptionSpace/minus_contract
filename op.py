88c219566531515e241e05853faeb0c6df6f87a6
KJ <kernel1983@gmail.com>
2024-01-21 22:27:08 +0800

    more functions

--- op.py
+++ op.py
@@ -2,8 +2,17 @@ import json
 
 import state
 
+
+def bridge(sender, d):
+    assert d['f'] == 'bridge'
+
+def hop(sender, d):
+    assert d['f'] == 'hop'
+
+# def deploy(sender, d):
+#     assert d['f'] == 'deploy'
+
 def mint(sender, d):
-    assert d['p'] == 'minus'
     assert d['f'] == 'mint'
     # assert len(d['tick']) == 4
     # assert int(d['max']) > 0
@@ -13,8 +22,8 @@ def mint(sender, d):
     state.state.setdefault(sender, 0)
     state.state[sender] += int(d['amt'])
 
+# assets related
 def transfer(sender, d):
-    assert d['p'] == 'minus'
     assert d['f'] == 'transfer'
     # assert len(d['tick']) == 4
     assert int(d['amt']) > 0
@@ -23,9 +32,47 @@ def transfer(sender, d):
     state.state.setdefault(d['to'], 0)
     state.state[d['to']] += int(d['amt'])
 
+def transfer_from(sender, d):
+    assert d['f'] == 'transfer_from'
+
+def approve(sender, d):
+    assert d['f'] == 'approve'
+
+# handle related
+def handle_reg(sender, d):
+    assert d['f'] == 'handle_reg'
+
+def resolve(sender, d):
+    assert d['f'] == 'resolve'
+
+def reverse(sender, d):
+    assert d['f'] == 'reverse'
+
+def mint(sender, d):
+    assert d['f'] == 'mint'
+
+# committee
+def committee_add_member(sender, d):
+    assert d['f'] == 'committee_add_member'
+    state.state.setdefault('committee_propose_%s' % d['args'][0], [sender])
+
+def committee_remove_member(sender, d):
+    assert d['f'] == 'committee_remove_member'
+
+def code_propose(sender, d):
+    assert d['f'] == 'code_propose'
+
+def code_vote(sender, d):
+    assert d['f'] == 'code_vote'
+
+def tick_propose(sender, d):
+    assert d['f'] == 'tick_propose'
+
+def tick_vote(sender, d):
+    assert d['f'] == 'tick_vote'
+
 def process(sender, arg):
-    # d = json.loads(s)
-    # print(s)
+    assert d['p'] == 'minus'
     if arg.get('f') == 'mint':
         mint(sender, arg)
     elif arg.get('f') == 'transfer':
